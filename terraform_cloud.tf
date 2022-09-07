20241d371f2a1d8c89a602f0805085ec6ae287ee
Chong Junn Teo <chongjunn92@gmail.com>
2022-09-07 22:37:06 +0800

    Create terraform_cloud.tf

---
+++ terraform_cloud.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.34.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+  project     = var.project
+}
+
+resource "google_compute_instance" "vm" {
+  zone         = us-central1-a
+  name         = "vm-test-tf-cloud"
+  machine_type = "e2-medium"
+  }
